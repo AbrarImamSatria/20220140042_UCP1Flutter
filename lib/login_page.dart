8f6fcf9e0b0d4579cdda3354e3150fbc8e8c02db
abrarimamsatria09 <092004abrar@gmail.com>
2025-04-25 09:39:13 +0700

    Membuat halaman login page statefull

---
+++ lib/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          ])
+        ),
+      ),
+    );
+  }
+}
