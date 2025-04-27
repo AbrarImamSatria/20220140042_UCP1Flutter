91c017ac0c4fa988e2de7bbb44f53345ea801340
abrarimamsatria09 <092004abrar@gmail.com>
2025-04-27 12:40:08 +0700

    membuat tampilan app bar home

---
+++ lib/home_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1_flutter/login_page.dart';
+
+class HomePage extends StatelessWidget {
+  final String email;
+
+  const HomePage({super.key, required this.email});
+
+  @override
+  Widget build(BuildContext context) {
+    String username = email.split('@')[0];
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1BA0E2),
+        elevation: 0,
+        title: Row(
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              margin: const EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/who.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Selamat Datang',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  username,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: IconButton(
+              icon: Icon(Icons.logout, color: Colors.white, size: 28),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                  (route) => false,
+                );
+              },
+            ),
+          ),
+        ],
+        toolbarHeight: 90,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
