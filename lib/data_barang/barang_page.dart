636c96f9dc8f7aa3fe6212ee3982487561464a47
abrarimamsatria09 <092004abrar@gmail.com>
2025-04-27 17:00:12 +0700

    membuat app bar pendataan barang

---
+++ lib/data_barang/barang_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BarangPage extends StatefulWidget {
+  const BarangPage({super.key});
+
+  @override
+  State<BarangPage> createState() => _BarangPageState();
+}
+
+class _BarangPageState extends State<BarangPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1BA0E2),
+        elevation: 0,
+        title: const Text(
+          'Pendataan Barang',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        toolbarHeight: 70,
+      ),
+    );
+  }
+}
