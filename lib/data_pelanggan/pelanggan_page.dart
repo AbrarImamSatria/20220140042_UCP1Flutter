c26ef23a164a096a7d9de7c17c535eef3dd2b75c
abrarimamsatria09 <092004abrar@gmail.com>
2025-04-27 15:07:52 +0700

    membuat app bar data pelanggan

---
+++ lib/data_pelanggan/pelanggan_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class PelangganPage extends StatefulWidget {
+  const PelangganPage({super.key});
+
+  @override
+  State<PelangganPage> createState() => _PelangganPageState();
+}
+
+class _PelangganPageState extends State<PelangganPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1BA0E2),
+        elevation: 0,
+        title: const Text(
+          'Data Pelanggan',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        toolbarHeight: 70,
+      ),
+    );
+  }
+}
