3b2f92b14d988ad3f6a3a65edb82dcd123f3464d
abrarimamsatria09 <092004abrar@gmail.com>
2025-04-27 15:04:24 +0700

    membuat tampilan detail piket

--- lib/data_piket/detail_piket_page.dart
+++ lib/data_piket/detail_piket_page.dart
@@ -35,6 +35,63 @@ class DetailPiketPage extends StatelessWidget {
         ),
         toolbarHeight: 70,
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    tanggal, // Menggunakan tanggal dari PiketPage
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF1BA0E2),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1BA0E2),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text(
+                      nama, // Menggunakan nama dari PiketPage
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              padding: const EdgeInsets.all(17),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: const Color(0xFF1BA0E2),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Text(
+                tugas, // Menggunakan tugas dari PiketPage
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            // Area kosong untuk konten tambahan jika diperlukan
+            const SizedBox(height: 500),
+          ],
+        ),
+      ),
     );
   }
 }
