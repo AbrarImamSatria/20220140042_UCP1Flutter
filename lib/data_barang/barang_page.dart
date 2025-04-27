25b01ed4e43b03dca64eea221210f1a27c5c8a95
abrarimamsatria09 <092004abrar@gmail.com>
2025-04-27 17:18:28 +0700

    membuat controller untuk textfield dan dropdown baranv

--- lib/data_barang/barang_page.dart
+++ lib/data_barang/barang_page.dart
@@ -1,36 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class BarangPage extends StatefulWidget {
   const BarangPage({super.key});
-
   @override
   State<BarangPage> createState() => _BarangPageState();
 }
 
 class _BarangPageState extends State<BarangPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController tanggalController = TextEditingController();
+  final TextEditingController jumlahBarangController = TextEditingController();
+  final TextEditingController hargaSatuanController = TextEditingController();
+
+  String? jenisTransaksi;
+  String? jenisBarang;
+
+  final List<String> jenisTransaksiOptions = ['Barang Masuk', 'Barang Keluar'];
+  final List<String> jenisBarangOptions = [
+    'Carrier',
+    'Sleeping Bag',
+    'Tenda',
+    'Sepatu',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting('id_ID', null);
+  }
+
+  String formatTanggalIndonesia(DateTime date) {
+    final DateFormat formatter = DateFormat('EEEE, d MMMM yyyy', 'id_ID');
+    return formatter.format(date);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1BA0E2),
-        elevation: 0,
-        title: const Text(
-          'Pendataan Barang',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w600,
-            fontSize: 20,
-          ),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        toolbarHeight: 70,
-      ),
-    );
+    return Scaffold();
   }
 }
