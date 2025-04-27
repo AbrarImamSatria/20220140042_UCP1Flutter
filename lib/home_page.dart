d63f471ce04802b0ef853d9571ed07247568fdb5
abrarimamsatria09 <092004abrar@gmail.com>
2025-04-27 13:36:21 +0700

    membuat menu data piket dan pelanggan

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ucp1_flutter/login_page.dart';
+import 'package:ucp1_flutter/pelanggan_page.dart';
+import 'package:ucp1_flutter/piket_page.dart';
 
 class HomePage extends StatelessWidget {
   final String email;
@@ -76,6 +78,94 @@ class HomePage extends StatelessWidget {
               child: Image.asset(
                 'assets/images/travelokaa.png',
                 fit: BoxFit.cover,
+              ),
+            ),
+
+            const SizedBox(height: 35.0),
+
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PiketPage(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: 175,
+                            margin: EdgeInsets.all(6),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF1BA0E2),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.task, color: Colors.white, size: 40),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Data Piket',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PelangganPage(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: 175,
+                            margin: EdgeInsets.all(6),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF1BA0E2),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.grid_view,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Data Pelanggan',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
